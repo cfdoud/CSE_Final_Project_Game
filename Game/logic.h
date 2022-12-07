73b534c7b1a50a66bf1814083f5888268ddd020e
Fin-NewWin <nguyenphien17@yahoo.com>
2022-12-06 23:27:26 -0800

    feat: updated collision

--- Game/logic.h
+++ Game/logic.h
@@ -3,11 +3,8 @@
 
 #include <cmath>
 
-bool collision(int playerRadius, int aiRaidius, float* playerVector, float* aiVector) {
-    if (fabs(aiVector[0] - playerVector[0]) <= (playerRadius + aiRaidius + aiRaidius) && fabs(aiVector[1] - playerVector[1]) <= (playerRadius + aiRaidius)) {
-        return true;
-    }
-    return false;
+static bool collision(int playerRadius, int aiRaidius, float* playerVector, float* aiVector) {
+    return (fabs(aiVector[0] - playerVector[0]) <= (playerRadius + aiRaidius + aiRaidius) && fabs(aiVector[1] - playerVector[1]) <= (playerRadius + aiRaidius));
 }
 
 #endif
